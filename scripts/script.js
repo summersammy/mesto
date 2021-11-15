5b43d7c98effbb5ee90f14bf193f5f518df0a594
Anastasiia Kraiz <sammy35kraiz@gmail.com>
2021-11-15 12:50:16 +0700

    5 спринт

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,143 @@
 const pageElement = document.querySelector('.page');
-const currentNameElement = pageElement.querySelector('.profile__info-title');
+
+// Profile
+const currentProfileNameElement = pageElement.querySelector('.profile__info-title');
 const currentProfessionElement = pageElement.querySelector('.profile__info-subtitle');
-const openPopupButton = document.querySelector('.profile__edit-button');
-const popupElement = document.querySelector('.popup');
-const closePopupButton = popupElement.querySelector('.popup__close');
-const form = popupElement.querySelector('form');
-const nameElement = popupElement.querySelector('#input-name');
-const professionElement = popupElement.querySelector('#input-profession');
-const hearts = document.querySelectorAll('.elements__heart');
+const openEditPopupButton = document.querySelector('.profile__edit-button');
 
-// for (let i = 0; i < hearts.length; i++) {
-//     const currentHeart = hearts[i];
-//     currentHeart.addEventListener('click', function (event) {
-//         event.target.classList.toggle('elements__heart-black');
-//     })
-// };
+// Popup
 
+const popup = document.querySelector('.popup');
+const popupContainer = popup.querySelector('.popup__container');
+const popupContent = popup.querySelector('.popup__content');
+const closePopupButton = popup.querySelector('.popup__close');
+
+// Card
+const openAddPopupButton = document.querySelector('.profile__add-button');
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.elements__list');
+
+// Templates
+const editTemplate = document.querySelector('#edit_name_template').content;
+const addPlaceTemplate = document.querySelector('#add_place_template').content;
+const imageViewTemplate = document.querySelector('#image_view_template').content;
+
+const initialCards = [
+    {
+        name: 'Алтай',
+        link: 'https://images.unsplash.com/photo-1593948360735-4ddb6f0dde7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://images.unsplash.com/photo-1501675423372-9bfa95849e62?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80'
+    },
+    {
+        name: 'Эльбрус',
+        link: 'https://images.unsplash.com/photo-1626518139514-65676cf25bac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://images.unsplash.com/photo-1537690381844-9da2b0b69640?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2129&q=80'
+    },
+    {
+        name: 'Карачаево-Черкесия',
+        link: 'https://images.unsplash.com/photo-1588584922681-745a2223f72c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1674&q=80'
+    },
+    {
+        name: 'Териберка',
+        link: 'https://images.unsplash.com/photo-1612860640446-3023ebe28e85?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2831&q=80'
+    }
+];
 
 function closePopup() {
-    popupElement.classList.remove('popup_opened');
+    setTimeout(function () {
+        popupContainer.classList.remove('popup__container-form');
+        popupContent.innerHTML = '';
+    }, 400);
+    popup.classList.remove('popup_opened', 'popup__image-overlay');
 }
 
-function openPopup() {
-    nameElement.value = currentNameElement.textContent;
+function openEditPopup() {
+    const editProfileForm = editTemplate.querySelector('form').cloneNode(true);
+    popupContainer.classList.add('popup__container-form');
+    popupContent.append(editProfileForm);
+
+    const nameElement = editProfileForm.querySelector('#input-name');
+    const professionElement = editProfileForm.querySelector('#input-profession');
+
+    nameElement.value = currentProfileNameElement.textContent;
     professionElement.value = currentProfessionElement.textContent;
-    popupElement.classList.add('popup_opened');
+
+    editProfileForm.addEventListener('submit', function editProfile(evt) {
+        evt.preventDefault();
+        currentProfileNameElement.textContent = nameElement.value;
+        currentProfessionElement.textContent = professionElement.value;
+        closePopup();
+    });
+    popup.classList.add('popup_opened');
 };
 
-form.addEventListener('submit', function editProfile(evt) {
-    evt.preventDefault();
-    currentNameElement.textContent = nameElement.value;
-    currentProfessionElement.textContent = professionElement.value;
-    closePopup();
+
+function openImagePopup(event) {
+    const imagePopupContent = imageViewTemplate.cloneNode(true);
+    const image = imagePopupContent.querySelector('img');
+    const caption = imagePopupContent.querySelector('p');
+
+    image.src = event.target.src;
+    caption.textContent = event.target
+        .closest('.elements__list-item')
+        .querySelector('.elements__text')
+        .textContent;
+    popupContent.append(imagePopupContent);
+    popup.classList.add('popup_opened', 'popup__image-overlay');
+}
+
+
+
+function addCard(name, imageLink) {
+    const cardElement = cardTemplate.querySelector('.elements__list-item').cloneNode(true);
+    const image = cardElement.querySelector('.elements__item');
+
+    image.src = imageLink;
+    cardElement.querySelector('.elements__text').textContent = name;
+
+    const heart = cardElement.querySelector('.elements__heart');
+    heart.addEventListener('click', function (event) {
+        event.target.classList.toggle('elements__heart-black');
+    })
+
+    image.addEventListener('click', openImagePopup)
+    const trash = cardElement.querySelector('.elements__trash');
+    trash.addEventListener('click', function (event) {
+        event.target.closest('.elements__list-item').remove();
+    })
+
+    cardList.prepend(cardElement);
+}
+
+
+
+initialCards.forEach(function (item) {
+    addCard(item.name, item.link);
 });
 
-openPopupButton.addEventListener('click', openPopup);
+function openAddPopup() {
+    const addPlaceForm = addPlaceTemplate.querySelector('form').cloneNode(true);
+    popupContainer.classList.add('popup__container-form');
 
+    const placeName = addPlaceForm.querySelector('#input-place-name');
+    const placeImageUrl = addPlaceForm.querySelector('#input-place-image-url');
+
+    popupContent.append(addPlaceForm);
+
+    addPlaceForm.addEventListener('submit', function addPlace(evt) {
+        evt.preventDefault();
+        addCard(placeName.value, placeImageUrl.value);
+        closePopup();
+    })
+    popup.classList.add('popup_opened');
+}
+
+openEditPopupButton.addEventListener('click', openEditPopup);
+openAddPopupButton.addEventListener('click', openAddPopup);
 closePopupButton.addEventListener('click', closePopup);
-
-
